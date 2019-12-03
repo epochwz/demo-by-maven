0243f6f777c4eacb2136da85e6791022e4238c93
epochwz <epoch.wz@gmail.com>
2019-12-03 17:19:56 +0800

    并发编程：线程基础 -- 停止线程 -- 演示 使用中断机制停止线程 -- 线程正常运行期间 -- 无效的中断

--- src/main/java/fun/epoch/learn/javase/multithread/stop/StopWithoutBlocking.java
+++ src/main/java/fun/epoch/learn/javase/multithread/stop/StopWithoutBlocking.java
@@ -4,7 +4,7 @@ package fun.epoch.learn.javase.multithread.stop;
  * 描述：演示使用中断机制在线程正常运行期间停止线程 (run 方法中没有使用 sleep / wait 等阻塞线程的方法)
  */
 public class StopWithoutBlocking {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread T = new Thread(() -> {
             for (int i = 0; i < Integer.MAX_VALUE / 2; i++) {
                 if (i % 10000 == 0) {
@@ -14,5 +14,7 @@ public class StopWithoutBlocking {
             System.out.println("线程正常执行完毕");
         });
         T.start();
+        Thread.sleep(1000);
+        T.interrupt(); // 被通知中断的线程如果没有做相应的中断响应处理，则中断不会产生任何效果
     }
 }
