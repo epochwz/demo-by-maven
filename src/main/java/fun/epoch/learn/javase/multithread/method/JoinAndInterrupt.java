b0b2ccfcdb456c7d8292981d20bc5530df11b7cf
epochwz <epoch.wz@gmail.com>
2019-12-10 13:33:17 +0800

    并发编程：线程基础 -- 常用方法 -- 演示 如何在 join 的中断响应处理中传递中断信号

--- src/main/java/fun/epoch/learn/javase/multithread/method/JoinAndInterrupt.java
+++ src/main/java/fun/epoch/learn/javase/multithread/method/JoinAndInterrupt.java
@@ -5,6 +5,7 @@ package fun.epoch.learn.javase.multithread.method;
  * <p>
  * 1. xx.join 抛出的中断异常需要当前线程 (调用 xx.join 的线程) 来处理 (响应), 而不是 xx
  * 2. 想要 xx.join 抛出中断异常，需要通知的是当前线程 (调用 xx.join 的线程), 而不是 xx
+ * 3. 演示：主线程如果在 join (等待子线程执行完毕) 的期间被中断，如何将中断传递给子线程
  */
 public class JoinAndInterrupt {
     public static void main(String[] args) {
@@ -36,6 +37,8 @@ public class JoinAndInterrupt {
             System.out.println(subThread.getName() + " 运行完毕，" + mainThread.getName() + " 执行完毕");
         } catch (InterruptedException e) {
             System.out.println(mainThread.getName() + " 正在等待 " + subThread.getName() + " 的期间被中断了！");
+            System.out.println(mainThread.getName() + " 被中断后通知 " + subThread.getName() + " 停止执行......");
+            subThread.interrupt(); // 3. 中断子线程
         }
     }
 }
