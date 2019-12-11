0d522030685c782907d889479af0b156b22213e7
epochwz <epoch.wz@gmail.com>
2019-12-11 17:15:30 +0800

    并发编程：线程基础 -- 异常处理 -- 演示 如何正确处理线程中的未捕获异常 -- 使用 try/catch 对每个子线程手动捕获和处理

--- src/main/java/fun/epoch/learn/javase/multithread/exception/ShowExceptionInChildThreadCantCatchDirectly.java
+++ src/main/java/fun/epoch/learn/javase/multithread/exception/ShowExceptionInChildThreadCantCatchDirectly.java
@@ -8,21 +8,22 @@ package fun.epoch.learn.javase.multithread.exception;
  * <p>
  * 结果执行时发现并不会打印出 “捕获到子线程抛出的异常”，而且其余线程也都照常执行
  * 证明 无法使用传统方式 (try/catch) 在主线程中捕获子线程中的异常
+ * <p>
+ * 3. 手动在每个子线程中捕获并处理异常 (麻烦，不推荐)
  */
 public class ShowExceptionInChildThreadCantCatchDirectly {
     public static void main(String[] args) {
-        try {
-            new Thread(runnable, "A").start();
-            new Thread(runnable, "B").start();
-            new Thread(runnable, "C").start();
-            new Thread(runnable, "D").start();
-        } catch (RuntimeException e) {
-            System.out.println("捕获到子线程抛出的异常：" + e.getMessage());
-        }
-
+        new Thread(runnable, "A").start();
+        new Thread(runnable, "B").start();
+        new Thread(runnable, "C").start();
+        new Thread(runnable, "D").start();
     }
 
     private static final Runnable runnable = () -> {
-        throw new RuntimeException("线程 " + Thread.currentThread().getName() + " 抛出了异常");
+        try {
+            throw new RuntimeException("线程 " + Thread.currentThread().getName() + " 抛出了异常");
+        } catch (RuntimeException e) {
+            System.out.println("捕获到子线程抛出的异常：" + e.getMessage());
+        }
     };
 }
