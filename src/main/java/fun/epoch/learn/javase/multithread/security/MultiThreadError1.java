c707e486854d2192c1500ff967f9f152edd36b0c
epochwz <epoch.wz@gmail.com>
2019-12-12 09:58:42 +0800

    并发编程：线程安全 -- 演示 i++ 结果出错 -- 寻找出错位置 -- 输出实际执行次数

--- src/main/java/fun/epoch/learn/javase/multithread/security/MultiThreadError1.java
+++ src/main/java/fun/epoch/learn/javase/multithread/security/MultiThreadError1.java
@@ -1,15 +1,19 @@
 package fun.epoch.learn.javase.multithread.security;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 描述：演示 线程安全问题 -- 运行结果出错
  */
 public class MultiThreadError1 {
     private static class Counter extends Thread {
         private static int count = 0;
+        private static AtomicInteger realCount = new AtomicInteger(0);
 
         @Override
         public void run() {
             for (int i = 0; i < 10000; i++) {
+                realCount.incrementAndGet();
                 count++;
             }
         }
@@ -25,6 +29,7 @@ public class MultiThreadError1 {
         counter1.join();
         counter2.join();
 
-        System.out.println(Counter.count);
+        System.out.println("线程实际执行次数：" + Counter.realCount);
+        System.out.println("线程表面执行次数：" + Counter.count);
     }
 }
