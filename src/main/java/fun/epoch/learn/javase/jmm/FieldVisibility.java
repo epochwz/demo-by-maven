3637065e8cad1a2e40f037191bbd38cd69b3f7db
epochwz <epoch.wz@gmail.com>
2019-12-14 19:56:39 +0800

    并发编程：内存模型 -- 可见性 -- 演示 volatile 可适用的场景 -- 作为内存同步的触发器

--- src/main/java/fun/epoch/learn/javase/jmm/FieldVisibility.java
+++ src/main/java/fun/epoch/learn/javase/jmm/FieldVisibility.java
@@ -4,7 +4,7 @@ package fun.epoch.learn.javase.jmm;
  * 描述：演示 可见性 问题
  */
 public class FieldVisibility {
-    private volatile int x = 1;
+    private int x = 1;
     private volatile int y = 2;
 
     private void change() {
@@ -14,7 +14,11 @@ public class FieldVisibility {
 
     // 当 x=1,y=3 时，证明出现了可见性问题
     private boolean output() {
-        if (x == 1 && y == 3) {
+        // volatile 变量 y 可以作为轻量级的内存同步触发器
+        // 1. 程序在读取 y 之前会将对 y 进行的赋值操作及其之前的所有操作 (对 x 进行赋值) 的结果同步回主内存
+        // 2. 因此当该线程看见 y=x=3 时，必然也能看见 x=3 这个操作
+        // 因此此处的判断条件永远不可能成功，也就是不存在可见性问题了
+        if (y == 3 && x == 1) {
             System.out.println(String.format("出现了可见性问题：(%s,%s)", x, y));
             return false;
         }
