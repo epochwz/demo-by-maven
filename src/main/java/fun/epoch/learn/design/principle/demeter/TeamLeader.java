91d52cff540b59a0db787e916cd1702325ec5dcc
epochwz <epoch.wz@gmail.com>
2019-12-17 22:57:14 +0800

    设计模式：最少知道原则 -- 符合迪米特法则

--- src/main/java/fun/epoch/learn/design/principle/demeter/TeamLeader.java
+++ src/main/java/fun/epoch/learn/design/principle/demeter/TeamLeader.java
@@ -1,9 +1,18 @@
 package fun.epoch.learn.design.principle.demeter;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class TeamLeader {
-    public void checkNumberOfCourse(List<Course> courses) {
+    private ArrayList<Course> courses;
+
+    {
+        this.courses = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            courses.add(new Course());
+        }
+    }
+
+    public void checkNumberOfCourse() {
         System.out.println(String.format("统计结果：一共有 %s 个在线课程", courses.size()));
     }
 }
