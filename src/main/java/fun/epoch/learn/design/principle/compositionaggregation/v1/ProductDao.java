d62bbf8700c97e57ccaa3b85b545a6d1a9163f71
epochwz <epoch.wz@gmail.com>
2019-12-18 17:39:51 +0800

    设计模式：合成复用原则 -- 继承复用 -- 需求变更：违背开闭原则，扩展时需要修改应用层以外的其他现有代码

--- src/main/java/fun/epoch/learn/design/principle/compositionaggregation/v1/ProductDao.java
+++ src/main/java/fun/epoch/learn/design/principle/compositionaggregation/v1/ProductDao.java
@@ -1,6 +1,11 @@
 package fun.epoch.learn.design.principle.compositionaggregation.v1;
 
-public class ProductDao extends MySQLDBConnection {
+/**
+ * 需求变更：需要使用不同的数据库
+ * <p>
+ * 继承复用：违背开闭原则 -- 扩展时需要修改应用层以外的其他现有代码
+ */
+public class ProductDao extends PostgreSQLDBConnection {
     public void addProduct() {
         String connection = super.getConnection();
         System.out.println("使用 " + connection + " 添加商品");
