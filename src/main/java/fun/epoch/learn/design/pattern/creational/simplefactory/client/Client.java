fd17dff21c25b105f54423d5656d714f597c0791
epochwz <epoch.wz@gmail.com>
2019-12-19 18:40:28 +0800

    设计模式：创建型 -- 简单工厂 -- 代码优化：使用简单工厂统一创建实现类

--- src/main/java/fun/epoch/learn/design/pattern/creational/simplefactory/client/Client.java
+++ src/main/java/fun/epoch/learn/design/pattern/creational/simplefactory/client/Client.java
@@ -1,25 +1,19 @@
 package fun.epoch.learn.design.pattern.creational.simplefactory.client;
 
 import fun.epoch.learn.design.pattern.creational.simplefactory.Video;
-import fun.epoch.learn.design.pattern.creational.simplefactory.videos.JavaVideo;
-import fun.epoch.learn.design.pattern.creational.simplefactory.videos.PythonVideo;
+import fun.epoch.learn.design.pattern.creational.simplefactory.VideoFactory;
 
 /**
  * 需求描述：一星期中的每一天都需要录制不同的视频
  * <p>
  * 简单实现：直接在应用层中根据条件创建不同视频类的对象，将导致应用层多处代码直接依赖具体的实现类
  * 需求变更：需要交换 Java & Python 视频的录制条件 -- 由于多处应用层代码直接依赖于具体的实现类，因此全部都需要改动
+ * 简单工厂：将 “根据条件创建不同视频类的对象” 的选择逻辑转移到工厂类，使得所有应用层代码只需要依赖同一个工厂类即可
  */
 public class Client {
     public static void main(String[] args) {
         for (int day = 0; day < 7; day++) {
-            Video video = null;
-            if (day % 7 == 0) {
-                video = new PythonVideo();
-            }
-            if (day % 7 == 1) {
-                video = new JavaVideo();
-            }
+            Video video = VideoFactory.getVideo(day);
             if (video != null) {
                 video.record();
             }
--- src/main/java/fun/epoch/learn/design/pattern/creational/simplefactory/client/Client2.java
+++ src/main/java/fun/epoch/learn/design/pattern/creational/simplefactory/client/Client2.java
@@ -1,19 +1,12 @@
 package fun.epoch.learn.design.pattern.creational.simplefactory.client;
 
 import fun.epoch.learn.design.pattern.creational.simplefactory.Video;
-import fun.epoch.learn.design.pattern.creational.simplefactory.videos.JavaVideo;
-import fun.epoch.learn.design.pattern.creational.simplefactory.videos.PythonVideo;
+import fun.epoch.learn.design.pattern.creational.simplefactory.VideoFactory;
 
 public class Client2 {
     public static void main(String[] args) {
         for (int day = 0; day < 4; day++) {
-            Video video = null;
-            if (day % 7 == 0) {
-                video = new PythonVideo();
-            }
-            if (day % 7 == 1) {
-                video = new JavaVideo();
-            }
+            Video video = VideoFactory.getVideo(day);
             if (video != null) {
                 video.record();
             }
