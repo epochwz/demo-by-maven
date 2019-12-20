967bb71be5a5480cb1e40777ce5a5e175aab6975
epochwz <epoch.wz@gmail.com>
2019-12-20 17:03:59 +0800

    设计模式：创建型 -- 工厂方法 -- 代码优化：使用工厂方法将各个类对象的创建逻辑推迟到相对应的工厂实现类中去实现
    作用：使得应用层只需要指定具体的工厂实现类，就可以得到相应的对象

--- src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/VideoFactory.java
+++ src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/VideoFactory.java
@@ -1,60 +1,7 @@
 package fun.epoch.learn.design.pattern.creational.factorymethod;
 
-import fun.epoch.learn.design.pattern.creational.factorymethod.videos.JavaVideo;
-import fun.epoch.learn.design.pattern.creational.factorymethod.videos.PythonVideo;
-
 // 代码演进：各个对象的创建逻辑逐渐变得十分复杂，简单工厂日益臃肿，因此难以维护
-public class VideoFactory {
-    public static Video getVideo(int day) {
-        switch (day % 7) {
-            case 0:
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                return new PythonVideo();
-            case 1:
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-                // 各种复杂的数据预处理
-
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                // 各种复杂的初始化逻辑
-                return new JavaVideo();
-            default:
-                return null;
-        }
-    }
+// 代码优化：使用工厂方法将各个类对象的创建逻辑推迟到相对应的工厂实现类中去实现
+public abstract class VideoFactory {
+    public abstract Video getVideo();
 }
---
+++ src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/factory/JavaVideoFactory.java
@@ -0,0 +1,33 @@
+package fun.epoch.learn.design.pattern.creational.factorymethod.factory;
+
+import fun.epoch.learn.design.pattern.creational.factorymethod.Video;
+import fun.epoch.learn.design.pattern.creational.factorymethod.VideoFactory;
+import fun.epoch.learn.design.pattern.creational.factorymethod.videos.JavaVideo;
+
+public class JavaVideoFactory extends VideoFactory {
+    @Override
+    public Video getVideo() {
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+        // 各种复杂的数据预处理
+
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        // 各种复杂的初始化逻辑
+        return new JavaVideo();
+    }
+}
