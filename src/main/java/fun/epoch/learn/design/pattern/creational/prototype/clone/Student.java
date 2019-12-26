e6f8b467bcab34ff27927f0e9956bc44f0ddb739
epochwz <epoch.wz@gmail.com>
2019-12-26 16:36:45 +0800

    设计模式：创建型 -- 原型模式 -- 深拷贝

--- src/main/java/fun/epoch/learn/design/pattern/creational/prototype/clone/Student.java
+++ src/main/java/fun/epoch/learn/design/pattern/creational/prototype/clone/Student.java
@@ -16,6 +16,15 @@ public class Student implements Cloneable {
     private String sex;
     private Date birthday;
 
+    // 重写克隆方法：使用深拷贝
+    // 避免由于 原始对象和拷贝对象持有相同的引用类型变量 所导致的 原始对象数据被拷贝对象篡改 的问题
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Student student = (Student) super.clone();
+        student.birthday = (Date) student.birthday.clone();
+        return student;
+    }
+
     public static void main(String[] args) throws CloneNotSupportedException {
         Date birthday = new Date();
         Student student1 = new Student("小明", "男", birthday);
@@ -28,7 +37,7 @@ public class Student implements Cloneable {
         // 由于默认的克隆方法是浅拷贝，因此 student1 的引用类型变量 birthday 属性也会被更改
         student2.getBirthday().setTime(666666666666L);
 
-        System.out.println(student1); // 1990
+        System.out.println(student1); // 2019
         System.out.println(student2); // 1990
     }
 }
