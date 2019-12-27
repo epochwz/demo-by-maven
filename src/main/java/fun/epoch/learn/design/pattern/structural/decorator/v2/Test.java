e0cf2d6476d216963d9be9e35c8a57a627da170d
epochwz <epoch.wz@gmail.com>
2019-12-27 17:47:19 +0800

    设计模式：结构型 -- 装饰者模式 -- 需求扩展：基于 装饰者 实现

--- src/main/java/fun/epoch/learn/design/pattern/structural/decorator/v2/Test.java
+++ src/main/java/fun/epoch/learn/design/pattern/structural/decorator/v2/Test.java
@@ -7,11 +7,17 @@ package fun.epoch.learn.design.pattern.structural.decorator.v2;
  * 1. 煎饼
  * 2. 煎饼 + 鸡蛋
  * 3. 煎饼 + 香肠
+ * <p>
+ * 需求扩展：
+ * 4. 煎饼 + 鸡蛋 + 香肠
+ * <p>
+ * 基于 装饰者 实现：可以实现现有煎饼和配料的任意组合，仅当出现新的煎饼或配料时才需要创建相应的类，符合开闭原则
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println(new BatterCake());
         System.out.println(new EggDecorator(new BatterCake()));
         System.out.println(new SausageDecorator(new BatterCake()));
+        System.out.println(new SausageDecorator(new EggDecorator(new BatterCake())));
     }
 }
