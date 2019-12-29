7fa0fbc86c9757572c4db9358b5f0df1881f3048
epochwz <epoch.wz@gmail.com>
2019-12-29 09:20:12 +0800

    设计模式：行为型 -- 模板方法

---
+++ src/main/java/fun/epoch/learn/design/pattern/behavioral/templatemethod/Test.java
@@ -0,0 +1,9 @@
+package fun.epoch.learn.design.pattern.behavioral.templatemethod;
+
+public class Test {
+    public static void main(String[] args) {
+        new JavaCourse().makeCourse();
+        System.out.println();
+        new FECourse().makeCourse();
+    }
+}
