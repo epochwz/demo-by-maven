943d81ad7ea080930bdd5e363c508b59804e05fd
epochwz <epoch.wz@gmail.com>
2019-12-30 10:34:56 +0800

    设计模式：行为型 -- 观察者模式 -- 代码准备

---
+++ src/main/java/fun/epoch/learn/design/pattern/behavioral/observer/Course.java
@@ -0,0 +1,10 @@
+package fun.epoch.learn.design.pattern.behavioral.observer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Course {
+    private String courseName;
+}
