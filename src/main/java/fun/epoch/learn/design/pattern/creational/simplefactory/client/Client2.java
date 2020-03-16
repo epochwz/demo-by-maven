e81110f891bb8d7f4fa9e68b53da6b81455bc80b
epochwz <epoch.wz@gmail.com>
2020-03-16 15:27:54 +0800

    设计模式：创建型 -- 简单工厂 -- 需求实现：应用层直接依赖具体的实现类

---
+++ src/main/java/fun/epoch/learn/design/pattern/creational/simplefactory/client/Client2.java
@@ -0,0 +1,22 @@
+package fun.epoch.learn.design.pattern.creational.simplefactory.client;
+
+import fun.epoch.learn.design.pattern.creational.simplefactory.Video;
+import fun.epoch.learn.design.pattern.creational.simplefactory.videos.JavaVideo;
+import fun.epoch.learn.design.pattern.creational.simplefactory.videos.PythonVideo;
+
+public class Client2 {
+    public static void main(String[] args) {
+        for (int day = 0; day < 4; day++) {
+            Video video = null;
+            if (day % 7 == 0) {
+                video = new JavaVideo();
+            }
+            if (day % 7 == 1) {
+                video = new PythonVideo();
+            }
+            if (video != null) {
+                video.record();
+            }
+        }
+    }
+}
