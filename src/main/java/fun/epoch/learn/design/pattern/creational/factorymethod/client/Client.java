57c249d82fa5140f31404c763068fe8ef8bc9760
epochwz <epoch.wz@gmail.com>
2020-03-18 18:14:01 +0800

    设计模式：创建型 -- 工厂方法 -- 代码优化：使用工厂方法将各个类对象的创建逻辑推迟到相对应的工厂实现类中去实现
    作用：使得应用层只需要指定具体的工厂实现类，就可以得到相应的对象

--- src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/client/Client.java
+++ src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/client/Client.java
@@ -2,6 +2,8 @@ package fun.epoch.learn.design.pattern.creational.factorymethod.client;
 
 import fun.epoch.learn.design.pattern.creational.factorymethod.Video;
 import fun.epoch.learn.design.pattern.creational.factorymethod.VideoFactory;
+import fun.epoch.learn.design.pattern.creational.factorymethod.factory.JavaVideoFactory;
+import fun.epoch.learn.design.pattern.creational.factorymethod.factory.PythonVideoFactory;
 
 /**
  * 描述：一星期中的每一天都需要录制不同的视频
@@ -14,7 +16,15 @@ import fun.epoch.learn.design.pattern.creational.factorymethod.VideoFactory;
 public class Client {
     public static void main(String[] args) {
         for (int day = 0; day < 7; day++) {
-            Video video = VideoFactory.getVideo(day);
+            VideoFactory videoFactory = null;
+            if (day % 7 == 0) {
+                videoFactory = new JavaVideoFactory();
+            }
+            if (day % 7 == 1) {
+                videoFactory = new PythonVideoFactory();
+            }
+            if (videoFactory == null) continue;
+            Video video = videoFactory.getVideo();
             if (video != null) {
                 video.record();
             }
--- src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/client/Client1.java
+++ src/main/java/fun/epoch/learn/design/pattern/creational/factorymethod/client/Client1.java
@@ -2,11 +2,21 @@ package fun.epoch.learn.design.pattern.creational.factorymethod.client;
 
 import fun.epoch.learn.design.pattern.creational.factorymethod.Video;
 import fun.epoch.learn.design.pattern.creational.factorymethod.VideoFactory;
+import fun.epoch.learn.design.pattern.creational.factorymethod.factory.JavaVideoFactory;
+import fun.epoch.learn.design.pattern.creational.factorymethod.factory.PythonVideoFactory;
 
 public class Client1 {
     public static void main(String[] args) {
         for (int day = 0; day < 3; day++) {
-            Video video = VideoFactory.getVideo(day);
+            VideoFactory videoFactory = null;
+            if (day % 7 == 0) {
+                videoFactory = new JavaVideoFactory();
+            }
+            if (day % 7 == 1) {
+                videoFactory = new PythonVideoFactory();
+            }
+            if (videoFactory == null) continue;
+            Video video = videoFactory.getVideo();
             if (video != null) {
                 video.record();
             }
