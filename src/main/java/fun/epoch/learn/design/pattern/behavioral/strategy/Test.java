ef44a84bd3d0fdfb9b17049ff9da4f19c6eeba5b
epochwz <epoch.wz@gmail.com>
2020-03-19 19:45:39 +0800

    设计模式：行为型 -- 策略模式 -- 代码优化：使用简单工厂来获取策略类

--- src/main/java/fun/epoch/learn/design/pattern/behavioral/strategy/Test.java
+++ src/main/java/fun/epoch/learn/design/pattern/behavioral/strategy/Test.java
@@ -1,26 +1,16 @@
 package fun.epoch.learn.design.pattern.behavioral.strategy;
 
-import fun.epoch.learn.design.pattern.behavioral.strategy.promotionstrategy.LiJianPromotionStrategy;
-import fun.epoch.learn.design.pattern.behavioral.strategy.promotionstrategy.ManJianPromotionStrategy;
+import fun.epoch.learn.design.pattern.behavioral.strategy.promotionstrategy.PromotionStrategy;
+import fun.epoch.learn.design.pattern.behavioral.strategy.promotionstrategy.PromotionStrategyFactory;
 
 public class Test {
     public static void main(String[] args) {
         // 代码演进：需要按照日期选择促销策略
         for (int i = 0; i < 7; i++) {
             int promotionKey = (int) (Math.random() * 8);
-
-            PromotionActivity activity = null;
-
-            if (promotionKey == 0) {
-                activity = new PromotionActivity(new LiJianPromotionStrategy());
-            }
-            if (promotionKey == 1) {
-                activity = new PromotionActivity(new ManJianPromotionStrategy());
-            }
-
-            if (activity != null) {
-                activity.executePromotionStrategy();
-            }
+            PromotionStrategy promotionStrategy = PromotionStrategyFactory.getPromotionStrategy(promotionKey);
+            PromotionActivity activity = new PromotionActivity(promotionStrategy);
+            activity.executePromotionStrategy();
         }
     }
 }
