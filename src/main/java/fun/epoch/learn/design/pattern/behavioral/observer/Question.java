69e7eab58e775bc09efa97e080e0f586742e513e
epochwz <epoch.wz@gmail.com>
2020-03-20 22:04:22 +0800

    设计模式：行为型 -- 观察者模式 -- 代码准备

---
+++ src/main/java/fun/epoch/learn/design/pattern/behavioral/observer/Question.java
@@ -0,0 +1,11 @@
+package fun.epoch.learn.design.pattern.behavioral.observer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Question {
+    private String username;
+    private String content;
+}
---
+++ src/main/java/fun/epoch/learn/design/pattern/behavioral/observer/Teacher.java
@@ -0,0 +1,8 @@
+package fun.epoch.learn.design.pattern.behavioral.observer;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Teacher {
+    private String name;
+}
