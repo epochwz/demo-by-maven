6158e3d8d9ca6f3181a6f9b27629343ea38476c9
epochwz <epoch.wz@gmail.com>
2020-03-20 23:44:19 +0800

    设计模式：行为型 -- 观察者模式 -- 代码实现

--- src/main/java/fun/epoch/learn/design/pattern/behavioral/observer/Teacher.java
+++ src/main/java/fun/epoch/learn/design/pattern/behavioral/observer/Teacher.java
@@ -2,7 +2,17 @@ package fun.epoch.learn.design.pattern.behavioral.observer;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Observable;
+import java.util.Observer;
+
 @AllArgsConstructor
-public class Teacher {
+public class Teacher implements Observer {
     private String name;
+
+    @Override
+    public void update(Observable observed, Object arg) {
+        Course course = (Course) observed;
+        Question question = (Question) arg;
+        System.out.println(String.format("教师 %s 收到了 %s 同学在课程 %s 上的提问：%s", this.name, question.getUsername(), course.getCourseName(), question.getContent()));
+    }
 }
