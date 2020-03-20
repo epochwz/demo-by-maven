a129bbb5011e0af34e483d840321b570d7d0fe6d
epochwz <epoch.wz@gmail.com>
2020-03-20 17:00:53 +0800

    设计模式：结构型 -- 代理模式 -- 静态代理 -- 同一个静态代理类可以代理相同接口的不同实现类

--- src/main/java/fun/epoch/learn/design/pattern/structural/proxy/staticproxy/Test.java
+++ src/main/java/fun/epoch/learn/design/pattern/structural/proxy/staticproxy/Test.java
@@ -1,5 +1,6 @@
 package fun.epoch.learn.design.pattern.structural.proxy.staticproxy;
 
+import fun.epoch.learn.design.pattern.structural.proxy.EnhancedOrderServiceImpl;
 import fun.epoch.learn.design.pattern.structural.proxy.IOrderService;
 import fun.epoch.learn.design.pattern.structural.proxy.Order;
 import fun.epoch.learn.design.pattern.structural.proxy.OrderServiceImpl;
@@ -10,6 +11,19 @@ public class Test {
         IOrderService targetObject = new OrderServiceImpl();
         // 代理对象
         IOrderService proxyObject = new OrderServiceStaticProxy(targetObject);
+        // 使用代理
+        proxyObject.saveOrder(new Order((int) (Math.random() * 10), new Object()));
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println((i + 1) + "s");
+        }
+
+        // ！！注意：同一个静态代理类可以代理同一个接口的不同实现类
+
+        // 目标对象
+        targetObject = new EnhancedOrderServiceImpl();
+        // 代理对象
+        proxyObject = new OrderServiceStaticProxy(targetObject);
         // 使用代理
         proxyObject.saveOrder(new Order((int) (Math.random() * 10), new Object()));
     }
