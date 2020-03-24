d8febbd779a0e7f18468c5c92402c0b106a53546
epochwz <epoch.wz@gmail.com>
2020-03-25 06:55:42 +0800

    设计模式：单一职责原则 -- 方法 -- 单一职责

--- src/main/java/fun/epoch/learn/design/principle/singleresponsibility/methoddemo/Demo.java
+++ src/main/java/fun/epoch/learn/design/principle/singleresponsibility/methoddemo/Demo.java
@@ -1,12 +1,23 @@
 package fun.epoch.learn.design.principle.singleresponsibility.methoddemo;
 
 public class Demo {
-    // 该方法职责不单一，需要经常修改，且容易引入风险
+    // 将该方法可能需要进行的修改转移到职责单一的方法中去
     public static void doSomething(boolean flag) {
         if (flag) {
-            System.out.println("do something 1...");
+            doSomething1();
         } else {
-            System.out.println("do something 2...");
+            doSomething2();
         }
     }
+
+    // 职责单一
+    public static void doSomething1() {
+        System.out.println("do something 1...");
+    }
+
+    // 职责单一
+    public static void doSomething2() {
+        System.out.println("do something 2...");
+    }
+
 }
