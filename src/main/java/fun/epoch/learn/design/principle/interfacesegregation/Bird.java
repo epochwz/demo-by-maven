2876e54fe25511227888ddf00497e6add54a8b5b
epochwz <epoch.wz@gmail.com>
2020-03-25 06:22:26 +0800

    设计模式：接口隔离原则 -- 接口职责隔离

--- src/main/java/fun/epoch/learn/design/principle/interfacesegregation/Bird.java
+++ src/main/java/fun/epoch/learn/design/principle/interfacesegregation/Bird.java
@@ -1,6 +1,6 @@
 package fun.epoch.learn.design.principle.interfacesegregation;
 
-public class Bird implements IAnimalAction {
+public class Bird implements IEatAnimalAction, IFlyAnimalAction {
     @Override
     public void eat() {
 
@@ -11,8 +11,8 @@ public class Bird implements IAnimalAction {
 
     }
 
-    @Override
-    public void swim() {
-        // 接口的职责过重，从而导致其实现类依赖一些不需要的接口方法，产生大量 “空实现”
-    }
+    // @Override // 编译出错：接口职责隔离，无需实现不必要的接口方法
+    // public void swim() {
+    //     // 接口的职责过重，从而导致其实现类依赖一些不需要的接口方法，产生大量 “空实现”
+    // }
 }
