17840ce340fd5912293145ef70a21763ada443b1
epochwz <epoch.wz@gmail.com>
2020-03-25 04:35:30 +0800

    设计模式：合成复用原则 -- 组合复用 -- 需求变更：符合开闭原则，扩展时无需修改应用层以外的其他现有代码

--- src/main/java/fun/epoch/learn/design/principle/compositionaggregation/v2/Test.java
+++ src/main/java/fun/epoch/learn/design/principle/compositionaggregation/v2/Test.java
@@ -1,8 +1,13 @@
 package fun.epoch.learn.design.principle.compositionaggregation.v2;
 
+/**
+ * 需求变更：需要使用不同的数据库
+ * <p>
+ * 组合复用：符合开闭原则 -- 扩展时无需修改应用层以外的其他现有代码
+ */
 public class Test {
     public static void main(String[] args) {
-        DBConnection dbConnection = new MySQLDBConnection();
+        DBConnection dbConnection = new PostgreSQLDBConnection();
         ProductDao productDao = new ProductDao(dbConnection);
         productDao.addProduct();
     }
