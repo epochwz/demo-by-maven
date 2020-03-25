ada464f0138cdba2bf84e1bc4aa1876b2c9c6d30
epochwz <epoch.wz@gmail.com>
2020-03-25 15:57:32 +0800

    设计模式：单一职责原则 -- 多重职责 -- 代码分析：类的职责不单一，所以需要经常修改且修改时容易引入风险

--- src/main/java/fun/epoch/learn/design/principle/singleresponsibility/classdemo/v1/Bird.java
+++ src/main/java/fun/epoch/learn/design/principle/singleresponsibility/classdemo/v1/Bird.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 
 /**
  * 职责：描述鸟类的移动方式
+ * <p>
+ * 职责一：描述 会飞的鸟 的移动方式
+ * 职责二：描述 会走的鸟 的移动方式
+ * <p>
+ * 职责不单一：需要同时负责描述 会飞的鸟 和 会走的鸟 这两种鸟类的移动方式
+ * 经常要修改：后续可能还需要增加诸如 会游泳的鸟 (企鹅) 等各种类型的鸟类
  */
 @AllArgsConstructor
 public class Bird {
@@ -12,7 +18,7 @@ public class Bird {
     public void move() {
         if ("鸵鸟".equals(birdName)) {
             System.out.println(birdName + " 用脚走");
-        }
+        } // 此处缺少 else 逻辑：因为该类的职责不单一，所以需要经常修改，且修改时容易引入风险
         System.out.println(birdName + " 用翅膀飞");
     }
 }
