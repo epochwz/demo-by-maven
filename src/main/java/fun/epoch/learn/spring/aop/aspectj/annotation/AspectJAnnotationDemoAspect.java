d53b8cfd6c134bfa50f02290c2f858250feaa7aa
epochwz <epoch.wz@gmail.com>
2020-03-27 05:21:44 +0800

    Spring: AOP -- AspectJ -- Annotation -- 切面类

---
+++ src/main/java/fun/epoch/learn/spring/aop/aspectj/annotation/AspectJAnnotationDemoAspect.java
@@ -0,0 +1,13 @@
+package fun.epoch.learn.spring.aop.aspectj.annotation;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * AOP 的切面类
+ */
+@Component
+@Aspect
+public class AspectJAnnotationDemoAspect {
+
+}
