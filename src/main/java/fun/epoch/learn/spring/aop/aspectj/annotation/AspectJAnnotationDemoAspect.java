168895db18d4441575aeea351185ba45bd8a1529
epochwz <epoch.wz@gmail.com>
2020-03-27 05:33:08 +0800

    Spring: AOP -- AspectJ -- Annotation -- 切面类 -- 后置通知

--- src/main/java/fun/epoch/learn/spring/aop/aspectj/annotation/AspectJAnnotationDemoAspect.java
+++ src/main/java/fun/epoch/learn/spring/aop/aspectj/annotation/AspectJAnnotationDemoAspect.java
@@ -1,6 +1,7 @@
 package fun.epoch.learn.spring.aop.aspectj.annotation;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,5 +15,11 @@ public class AspectJAnnotationDemoAspect {
     @Before("execution(* *(..))")
     public void beforeAdvice(JoinPoint joinPoint) { // 可以使用方法参数 JoinPoint 对象获取切点信息
         System.out.println("\n========== 前置通知 ========== " + joinPoint);
+    }
+
+    @AfterReturning(value = "execution(* fun.epoch.learn.spring.aop..*(..))")
+    public void afterReturningAdvice() {
+        System.out.println("========== 后置通知 ==========");
+        System.out.println();
     }
 }
