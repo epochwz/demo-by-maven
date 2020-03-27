d9e6684ee7b49775120f5d0f1cba7559b694b87e
epochwz <epoch.wz@gmail.com>
2020-03-27 09:09:46 +0800

    Spring: AOP -- Proxy -- 动态代理 -- 代码准备 (CGLib)

---
+++ src/main/java/fun/epoch/learn/spring/aop/proxy/dynamic/cglib/Test.java
@@ -0,0 +1,13 @@
+package fun.epoch.learn.spring.aop.proxy.dynamic.cglib;
+
+import fun.epoch.learn.spring.aop.target.ProductDao;
+
+public class Test {
+    public static void main(String[] args) {
+        ProductDao productDao = new ProductDao();
+        productDao.save();
+        productDao.update();
+        productDao.find();
+        productDao.delete();
+    }
+}
