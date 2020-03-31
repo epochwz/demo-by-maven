58234b5e6e23f64fd6c058342ed993432dab144f
epochwz <epoch.wz@gmail.com>
2020-03-31 15:36:42 +0800

    框架：请求分发 -- 让 MappingHandler 通过反射调用来执行具体的请求处理逻辑

---
+++ framework/src/main/java/fun/epoch/framework/mini/spring/beans/BeanFactory.java
@@ -0,0 +1,7 @@
+package fun.epoch.framework.mini.spring.beans;
+
+public class BeanFactory {
+    public static Object getBean(Class<?> clazz) {
+        return null;
+    }
+}
--- framework/src/main/java/fun/epoch/framework/mini/spring/web/handler/MappingHandler.java
+++ framework/src/main/java/fun/epoch/framework/mini/spring/web/handler/MappingHandler.java
@@ -1,9 +1,50 @@
 package fun.epoch.framework.mini.spring.web.handler;
 
+import fun.epoch.framework.mini.spring.beans.BeanFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class MappingHandler {
+    private String uri;
+    private Class<?> clazz;
+    private Method method;
+    private String[] args;
+
+    public MappingHandler() {
+    }
+
+    public MappingHandler(String uri, Class<?> clazz, Method method, String[] args) {
+        this.uri = uri;
+        this.clazz = clazz;
+        this.method = method;
+        this.args = args;
+    }
+
     public void handle(HttpServletRequest request, HttpServletResponse response) {
+        if (uri.equals(request.getRequestURI())) {
+            try {
+                Object[] params = Arrays.stream(args).map(request::getParameter).toArray();
+                Object object = BeanFactory.getBean(clazz);
+                Object result = method.invoke(object, params);
+                response.getWriter().write(result.toString());
+            } catch (IllegalAccessException | InvocationTargetException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MappingHandler{" +
+                "uri='" + uri + '\'' +
+                ", clazz=" + clazz +
+                ", method=" + method +
+                ", args=" + Arrays.toString(args) +
+                '}';
     }
 }
