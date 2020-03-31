506b528d9959ae6ea1d55165645ab6572096aa32
epochwz <epoch.wz@gmail.com>
2020-03-31 18:25:03 +0800

    框架：启动时扫描 Class

--- framework/src/main/java/fun/epoch/framework/mini/spring/starter/MiniSpringApplication.java
+++ framework/src/main/java/fun/epoch/framework/mini/spring/starter/MiniSpringApplication.java
@@ -1,8 +1,12 @@
 package fun.epoch.framework.mini.spring.starter;
 
+import fun.epoch.framework.mini.spring.core.classscanner.ClassScanner;
 import fun.epoch.framework.mini.spring.web.server.TomcatServer;
 import fun.epoch.framework.mini.spring.web.servlet.DispatcherServlet;
 import org.apache.catalina.LifecycleException;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MiniSpringApplication {
     private static final int DEFAULT_PORT = 8080;
@@ -12,8 +16,12 @@ public class MiniSpringApplication {
             int port = args != null && args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
             new TomcatServer(port).addServlet("/", "DispatcherServlet", new DispatcherServlet()).start();
             System.out.println("启动 Tomcat 成功：正在监听 [localhost:" + port + "]");
+            List<Class<?>> classes = ClassScanner.scan(clazz.getPackage().getName());
+            classes.forEach(System.out::println);
         } catch (LifecycleException e) {
             System.out.println("启动 Tomcat 失败：" + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
