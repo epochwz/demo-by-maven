d129eb740a3d35fc817df67b63c71757af9f1507
epochwz <epoch.wz@gmail.com>
2020-04-01 16:12:32 +0800

    Spring: AOP -- Spring AOP -- 自动代理 -- 代码准备

---
+++ src/main/java/fun/epoch/learn/spring/aop/springaop/autoproxy/MyAfterAdvice.java
@@ -0,0 +1,12 @@
+package fun.epoch.learn.spring.aop.springaop.autoproxy;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+public class MyAfterAdvice implements AfterReturningAdvice {
+    @Override
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+        System.out.println("========== 后置通知 ==========");
+    }
+}
